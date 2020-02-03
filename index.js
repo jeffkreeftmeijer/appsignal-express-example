14e6f433bdf3c915a1641bf5da71c8627a1b036e
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-02-03 11:45:25 +0100

    Install AppSignal

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const { Appsignal } = require("@appsignal/nodejs")
+const { expressMiddleware } = require("@appsignal/express")
+
+const appsignal = new Appsignal({
+  active: true,
+  name: "appsignal-express-example",
+  apiKey: "00000000-0000-0000-0000-000000000000"
+})
 const app = express()
 const port = 3000
+
+app.use(expressMiddleware(appsignal))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
